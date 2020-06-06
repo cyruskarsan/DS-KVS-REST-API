306b8ee50b8d7031cfe402d24740ebb8e2112f0f
cyruskarsan <cyruskarsan@gmail.com>
2020-06-05 19:28:14 -0700

    added final contribution notes and mechanism description. Also added bash scripts for testing replicas

---
+++ replica4.sh
@@ -0,0 +1,1 @@
+docker run -p 8086:8085 --net=mynet --ip=10.10.0.5 --name="node4" -e SOCKET_ADDRESS="10.10.0.5:8085" -e VIEW="10.10.0.2:8085,10.10.0.3:8085,10.10.0.4:8085,10.10.0.5:8085" -e SHARD_COUNT="2" assignment4-img
